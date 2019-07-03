2d79c73fa967f81ba2f46a683d0a2505382d60c1
Max Antonucci <maxx1128@gmail.com>
2019-07-02 21:25:45 -0400

    Fixed puppeteer for Heroku

--- app/image.js
+++ app/image.js
@@ -5,8 +5,14 @@ const defaultViewport = {
   height: 700
 };
 
+{}
+
 const updateImage = async () => {
-  const browser = await puppeteer.launch({ defaultViewport: defaultViewport });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    defaultViewport: defaultViewport
+  });
   const page = await browser.newPage();
   await page.goto('https://zealous-mccarthy-bc8472.netlify.com/');
   await page.screenshot({path: 'app/quote.png'});
