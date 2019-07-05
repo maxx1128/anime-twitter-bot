04870b5545ab73799e472bb1390156f00a8c8fb0
Max Antonucci <maxx1128@gmail.com>
2019-07-05 19:45:54 -0400

    Remove external config import

--- app/tweet.js
+++ app/tweet.js
@@ -3,7 +3,15 @@ const Twit         = require('twit'),
       path         = require('path'),
       updateImage  = require('./image.js'),
       tweetContent = require('./tweet-content.js'),
-      config       = require('./../config');
+      dotenv       = require('dotenv');
+      dotenv.config();
+
+const config = {
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+}
 
 const T = new Twit(config);
 
